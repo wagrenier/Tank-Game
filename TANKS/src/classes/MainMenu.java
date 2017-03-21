d9381cc69445ba8ee942b239c0b1b1684e65348b
cedrikdubois <cedrik.dubois1998@gmail.com>
2017-03-20 20:24:52 -0400

    Textures for North Korean Tank
    Body facing right
    Body facing left
    Cannon

--- TANKS/src/classes/MainMenu.java
+++ TANKS/src/classes/MainMenu.java
@@ -5,10 +5,24 @@
  */
 package classes;
 
+import javafx.scene.layout.Pane;
+
 /**
  *
  * @author Cedrik Dubois
  */
-public class MainMenu {
+public class MainMenu extends Pane{
+    
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 800;
+    
+    public MainMenu(){
+        this.setMaxHeight(HEIGHT);
+        this.setMinHeight(HEIGHT);
+        this.setMaxWidth(WIDTH);
+        this.setMinWidth(WIDTH);
+        
+    }
+    
     
 }
