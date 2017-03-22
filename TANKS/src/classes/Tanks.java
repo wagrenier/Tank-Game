f72640d5dacf44cb2b650bb8a5f3367ca95e6650
William Adam-Grenier <william.a-g@hotmail.com>
2017-03-22 07:58:58 -0400

    Implemented the textures for the tanks

--- TANKS/src/classes/Tanks.java
+++ TANKS/src/classes/Tanks.java
@@ -6,6 +6,8 @@
 package classes;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -14,22 +16,32 @@ import javafx.scene.shape.Rectangle;
  *
  * @author william
  */
-public class Tanks extends Rectangle{
+public class Tanks extends Circle{
     
-    
+    ImageView imageView;
     
     Tanks(){
-        //this.setRadius(15);
-        Image texture = new Image("Texture/Tanks/Canada/Body/Red_Tank.png", 1200, 800, false, false);
-        
+        this.setRadius(50);
+        Image texture = new Image("Texture/Tanks/Canada/Body/Red_Tank_(100x100).png");
+        imageView = new ImageView(texture);
+        this.setCenterY(-25);
+        //imageView.setClip(this);
+        //imageView.setX(this.getCenterX());
+        //imageView.setY(this.getCenterY() + 50);
         this.setFill(new ImagePattern(texture, 0, 0, 1, 1, true));
         //this.setLayoutX(100);
         //this.setLayoutY(100);
-        this.setWidth(1200);
-        this.setHeight(800);
+        //this.setWidth(100);
+        //this.setHeight(100);
         //setFill(Color.BLACK);
         //setStroke(Color.BLACK);
         setTranslateX(50);
         setTranslateY(200);
     }  
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+    
+    
 }
