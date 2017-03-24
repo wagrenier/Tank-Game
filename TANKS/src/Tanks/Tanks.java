2e7786faa2376e48097583595ff37ab1f46f0840
William Adam-Grenier <william.a-g@hotmail.com>
2017-03-24 10:33:23 -0400

    Updated file path

--- TANKS/src/Tanks/Tanks.java
+++ TANKS/src/Tanks/Tanks.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package classes;
+package Tanks;
 
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
